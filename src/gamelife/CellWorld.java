22d5690a23016ece8c9058d0552967796c82c0ab
RyannWilcox <ryanwilcox1@yahoo.com>
2014-06-15 16:45:47 -0400

    Changes to pattern action listener and new speed listener
    when choosing a cell pattern the generation count is now reset.  Also,
    new JMenu is added.  You can now select the speed that you iterate
    through game of life generations

--- src/gamelife/CellWorld.java
+++ src/gamelife/CellWorld.java
@@ -22,26 +22,27 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class CellWorld extends JPanel implements ActionListener {
 	private static final long serialVersionUID = 1L;
-	JFrame myFrame;
-	JButton[] button;
-	static final String[] BUTTON_STR = { "Go", "Step", "Stop", "Quit" };
-	private final int MAX_ROWS = 100;
-	private final int MAX_COLUMNS = 100;
+	private JFrame myFrame;
+	private JButton[] button;
+	private static final String[] BUTTON_STR = { "Go", "Step", "Stop","Clear","Quit" };
+	private static final int MAX_ROWS = 100;
+	private static final int MAX_COLUMNS = 100;
 
 	private GridSquare theGrid[][] = new GridSquare[MAX_ROWS][MAX_COLUMNS];
 	private Cell cell[][] = new Cell[MAX_ROWS][MAX_COLUMNS];
 	private CellUpdate updateCells = new CellUpdate();
-	private boolean running = true;
 	private JLabel generation = new JLabel("Gen: 1");
-	private int genCount = 1;
 	private JMenuBar menu = new JMenuBar();
 	
+	private boolean running = true;
+	private int genCount = 1;
 	private int newRow = 0; 
 	private int newCol = 0;
-	 
+	private int speed = 120;
 	private Updater updates = new Updater();
 
 	public CellWorld(String title, int width, int height) {
@@ -125,55 +126,142 @@ public class CellWorld extends JPanel implements ActionListener {
 		}
 		controlPanel.add(generation);
 		myFrame.add(controlPanel, BorderLayout.EAST);
-		
 		/*
 		 * Menu for selecting specific patterns
 		 */
 		myFrame.setJMenuBar(menu);
 		JMenu choices = new JMenu("Patterns");
+		JMenu speeds = new JMenu("Speeds");
+		
+		/*Patterns*/
 		JMenuItem gliderGun = new JMenuItem("GliderGun");
 		JMenuItem pulsar = new JMenuItem("Pulsar");
 		JMenuItem Pentomino = new JMenuItem("R-Pentomino");
+		JMenuItem spider = new JMenuItem("Spider");
+		JMenuItem custom = new JMenuItem("Custom");
+		
+		/*Speeds*/
+		JMenuItem fast = new JMenuItem("Fast");
+		JMenuItem med = new JMenuItem("Medium");
+		JMenuItem slow = new JMenuItem("Slow");
 		menu.add(choices);
+		menu.add(speeds);
 		choices.add(gliderGun);
 		choices.add(pulsar);
 		choices.add(Pentomino);
+		choices.add(spider);
+		choices.add(custom);
+		speeds.add(fast);
+		speeds.add(med);
+		speeds.add(slow);
 		
 		
 		/*
-		 * Clears the cells and then prints the pattern to the
-		 * grid.
+		 * Clears the cells and then prints the glider gun
+		 *  pattern to the grid.
 		 */
 		gliderGun.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				cell = updateCells.clearCells(cell, MAX_ROWS, MAX_COLUMNS);
 				cell = updateCells.createGliderGun(cell);
+				genCount = 1;
+				String genStr= Integer.toString(genCount);
+				/*update JLabel*/
+				generation.setText("Gen: "+genStr);
 				repaint();
 			}
 		});
 		
+		/* Clears the grid of all live cells. Then
+		 * creates the formation pulsar and paints the
+		 * grid with those cells.
+		 */
 		pulsar.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				cell = updateCells.clearCells(cell, MAX_ROWS, MAX_COLUMNS);
 				cell = updateCells.createPulsar(cell);
+				genCount = 1;
+				String genStr= Integer.toString(genCount);
+				/*update JLabel*/
+				generation.setText("Gen: "+genStr);
 				repaint();
 			}
 		});
 		
+		
+		/* Clears the grid of all live cells then
+		 * creates the formation R-Pentomino and
+		 * updates the grid    	
+		 */
 		Pentomino.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e){
 				cell = updateCells.clearCells(cell, MAX_ROWS, MAX_COLUMNS);
 				cell = updateCells.createRPentomino(cell);
+				genCount = 1;
+				String genStr= Integer.toString(genCount);
+				/*update JLabel*/
+				generation.setText("Gen: "+genStr);
 				repaint();
 			}
 		});
+		
+		spider.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cell = updateCells.clearCells(cell, MAX_ROWS, MAX_COLUMNS);
+				cell = updateCells.createSpider(cell);
+				genCount = 1;
+				String genStr= Integer.toString(genCount);
+				/*update JLabel*/
+				generation.setText("Gen: "+genStr);
+				repaint();
+			}
+		});
+		
+		custom.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cell = updateCells.clearCells(cell, MAX_ROWS, MAX_COLUMNS);
+				cell = updateCells.createCustom(cell);
+				genCount = 1;
+				String genStr= Integer.toString(genCount);
+				/*update JLabel*/
+				generation.setText("Gen: "+genStr);
+				repaint();
+			}	
+		});
+		
+		/*CHANGES THE SPEED OF THE THREAD!!!*/
+		med.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				speed = 120;
+			}
+		});
+		fast.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				speed = 70;
+			}
+		});
+		slow.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				speed = 180;
+			}
+		});
+		
+			
+		
 	}
 
 	/**
-	 * Paints the grid
+	 * Paints the grid with cell updates
 	 */
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
@@ -192,12 +280,16 @@ public class CellWorld extends JPanel implements ActionListener {
 		}
 	}
 
+	/**
+	 * Handles the action of pressing the buttons
+	 * in the control panel
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == button[0]) {
 			System.out.println("Start thread!");
-		    setDoubleBuffered(true);
 		    running = true;
+		    //Begin updating the grid
 		    Thread updateThread = new Thread(updates);
 			updateThread.start();
 		}
@@ -211,10 +303,16 @@ public class CellWorld extends JPanel implements ActionListener {
 
 		}
 		if (e.getSource() == button[2]) {
+			//Will stop the thread
 				running = false;
 		}
-
-		if (e.getSource() == button[3]) {
+		if(e.getSource() == button[3]){
+			cell = updateCells.clearCells(cell, MAX_ROWS, MAX_COLUMNS);
+			genCount = 1;
+			running = false;
+			repaint();
+		}
+		if (e.getSource() == button[4]) {
 			System.exit(1);
 		}
 	}
@@ -230,14 +328,17 @@ public class CellWorld extends JPanel implements ActionListener {
 		@Override
 		public void run() {
 			while (running) {
+				// While the stop button/quit button is not pushed
+				// the thread will continue to loop
 				cell = updateCells.updateSquares(cell, 100, 100);
+				/*update JLabel*/
 				genCount++;
 				String genStr= Integer.toString(genCount);
-				/*update JLabel*/
 				generation.setText("Gen: "+genStr);
+				
 				repaint();
 				try {
-					Thread.sleep(120);
+					Thread.sleep(speed);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();

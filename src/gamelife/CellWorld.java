3713f0f6efcbb11ecf8c1c82ace1a239d53a6f24
RyannWilcox <ryanwilcox1@yahoo.com>
2014-06-23 18:03:35 -0400

    Changing where objects are initialized

--- src/gamelife/CellWorld.java
+++ src/gamelife/CellWorld.java
@@ -5,8 +5,12 @@ import javax.swing.JFrame;
 
 
 public class CellWorld {
-	private JFrame myFrame;
+	private JFrame myFrame = new JFrame();
 	private CellsAndGrid cellAndGridSquare = new CellsAndGrid();
+	private GridPanel gridpanel = new GridPanel(cellAndGridSquare);
+	private ControlPanel controlpanel = new ControlPanel(gridpanel,cellAndGridSquare);
+	private ChoiceMenu menuChoices = new ChoiceMenu(gridpanel,controlpanel,cellAndGridSquare);
+	
 	public CellWorld(String title, int width, int height) {
 		super();
 	    layoutSetup(title, width, height);
@@ -21,19 +25,18 @@ public class CellWorld {
 	 * @param theHeight
 	 */
 	public void layoutSetup(String theTitle, int theWidth, int theHeight) {
-		myFrame = new JFrame(theTitle);
+		myFrame.setTitle(theTitle);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setSize(theWidth, theHeight);
 		myFrame.setLayout(new BorderLayout());
-		GridPanel gridpanel = new GridPanel(cellAndGridSquare);
+		
+		//panel to store the grid squares
 		myFrame.add(gridpanel);
 		
-		ControlPanel cp = new ControlPanel(gridpanel,cellAndGridSquare);
-		myFrame.add(cp,BorderLayout.EAST);
+		//panel for the buttons
+		myFrame.add(controlpanel,BorderLayout.EAST);
 		
-		
-		/* Menu for selecting specific patterns */
-		ChoiceMenu menuChoices = new ChoiceMenu(gridpanel,cp,cellAndGridSquare);
+		//JMenu for game customization
 		myFrame.setJMenuBar(menuChoices);
 	}
 }

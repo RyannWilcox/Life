a3f2eb745e4c0c90c1fabfbaeb84a173be92423f
RyannWilcox <ryanwilcox1@yahoo.com>
2014-08-22 12:45:39 -0400

    new interface created
    This holds the values for the max bounds of the grid and the min bounds
    of the grid

---
+++ src/gamelife/MaxMinBounds.java
@@ -0,0 +1,10 @@
+package gamelife;
+
+public interface MaxMinBounds {
+	/* 
+	 * Bounds used for checking the neighbors of
+ 	 * each square on the grid
+ 	 */
+	static final int MAX = 100;
+	static final int MIN = 0;
+}

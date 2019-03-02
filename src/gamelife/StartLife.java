b729fd2ad7b09b0f7424236a47159a90bb173943
RyannWilcox <rwilcox11@icloud.com>
2019-03-02 17:09:00 -0500

    Removed Static variable in the main

--- src/gamelife/StartLife.java
+++ src/gamelife/StartLife.java
@@ -2,12 +2,10 @@ package gamelife;
 
 public class StartLife {
 
-	public static CellWorld gameOfLifeBoard;
-
 	/* We launch the entire application from here...*/
 	public static void main(String[] args) {
 		System.out.println("Starting Conway's Game of Life");
 
-		gameOfLifeBoard = new CellWorld("Life", 1080, 800);
+		CellWorld gameOfLifeBoard = new CellWorld("Life", 1080, 800);
 	}
 }
